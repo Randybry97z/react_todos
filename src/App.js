164d00f640fa4c606daf365c4a6abb3a0e30af37
Bryan <bssandovalrod@gmail.com>
2020-09-15 07:09:04 -0500

    Saving todos in local

--- src/App.js
+++ src/App.js
@@ -3,17 +3,27 @@ import './App.css';
 
 function App() {
   const [todos, setTodos] = useState(['Take dogs for a walk', 'Take the rubbish out']);
+  const [input, setInput] = useState('');
+
+  const addTodo = (event) => {
+    //this will fire off when we click the button
+    event.preventDefault();//stop the refresh
+    setTodos([...todos, input]);
+
+  }
 
   return (
     <div className="App">
-      <h1>Hello world! 🔥 </h1>
-      <input />
-      <button>Add Todo</button>
+      <h1>Hello world!</h1>
+      <form>
+        <input value={input} onChange={event => setInput(event.target.value)} />
+        <button type="submit" onClick={addTodo}>Add Todo</button>
+      </form>
 
       <ul>
-        {todos.map(todo => {
-          <li>{todo}</li>
-        })}
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
       </ul>
     </div>
   );
