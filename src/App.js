0d8fc01b07bd849339baecefe99027d2cbf6be73
Bryan <bssandovalrod@gmail.com>
2020-09-15 07:31:38 -0500

    Adding material UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
 import './App.css';
+import Todo from './Todo';
 
 function App() {
   const [todos, setTodos] = useState(['Take dogs for a walk', 'Take the rubbish out']);
@@ -9,20 +11,25 @@ function App() {
     //this will fire off when we click the button
     event.preventDefault();//stop the refresh
     setTodos([...todos, input]);
-
+    setInput(''); //clear input after save
   }
 
   return (
     <div className="App">
       <h1>Hello world!</h1>
       <form>
-        <input value={input} onChange={event => setInput(event.target.value)} />
-        <button type="submit" onClick={addTodo}>Add Todo</button>
+        <FormControl>
+          <InputLabel>Write a Todo</InputLabel>
+          <Input value={input} onChange={event => setInput(event.target.value)} />
+        </FormControl>
+        <Button disabled={!input} type="submit" onClick={addTodo} variant="contained" color="primary">
+          Add Todo
+        </Button>
       </form>
 
       <ul>
         {todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
+          <Todo todo={todo} key={index} />
         ))}
       </ul>
     </div>
