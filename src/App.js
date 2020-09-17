23ec1a2f8961113803dd640e5081ec2d3dbadc92
Bryan <bssandovalrod@gmail.com>
2020-09-17 13:39:07 -0500

    adding delete functionality

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   useEffect(() => {
     //useEffect runs when the App.js loads
     db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
-      setTodos(snapshot.docs.map(doc => doc.data().todo))
+      setTodos(snapshot.docs.map(doc => ({ id: doc.id, todo: doc.data().todo })))
     })
   }, [input])
 
--- src/Todo.js
+++ src/Todo.js
@@ -1,13 +1,15 @@
-import { List, ListItem, ListItemText } from '@material-ui/core';
-import './Todo.css';
 import React from 'react';
+import { List, ListItem, ListItemText, Button } from '@material-ui/core';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever'
+import db from './firebase';
 
 function Todo(props) {
   return (
     <List className="todo__list">
       <ListItem>
-        <ListItemText primary={props.todo} secondary="It's time to work 🔥" />
+        <ListItemText primary={props.todo.todo} secondary="It's time to work 🔥" />
       </ListItem>
+      <DeleteForeverIcon onClick={event => db.collection('todos').doc(props.todo.id).delete()} />
     </List>
   )
 }
