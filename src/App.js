a814b810238e2285605cf273fd38ee7763831aa6
Bryan <bssandovalrod@gmail.com>
2020-09-17 12:58:34 -0500

    Deploy

--- src/App.js
+++ src/App.js
@@ -3,29 +3,37 @@ import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
 import './App.css';
 import Todo from './Todo';
 import db from './firebase';
+import firebase from 'firebase';
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
 
   //When the app loads, need to listen to the db and fetch new todos as they get added/removed
-  //useEffect runs when the App.js loads
   useEffect(() => {
-    db.collection('todos').onSnapshot(snapshot => {
+    //useEffect runs when the App.js loads
+    db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
       setTodos(snapshot.docs.map(doc => doc.data().todo))
     })
   }, [input])
 
   const addTodo = (event) => {
     //this will fire off when we click the button
-    event.preventDefault();//stop the refresh
+    event.preventDefault(); //stop the refresh
+
+    // add todo into db
+    db.collection('todos').add({
+      todo: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    });
+
     setTodos([...todos, input]);
     setInput(''); //clear input after save
   }
 
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <h1>Hello Randy!</h1>
       <form>
         <FormControl>
           <InputLabel>✅ Write a Todo</InputLabel>
